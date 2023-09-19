9e87927aa13c33d374167cd037a1dd8f8c081bf1
AbhishekR14 <aranjan0288@gmail.com>
2023-09-19 11:49:38 +0530

    strating page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import React from 'react'
-import Dice from './dice.jsx'
 function App() {
-
   return (
     <>
       <main>
         <h1 className="title">Tenzies</h1>
-        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-        <div className="dice-grid">
-        <Dice value="1"/>
-        <Dice value="2"/>
-        <Dice value="3"/>
-        <Dice value="4"/>
-        <Dice value="5"/>
-        <Dice value="6"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        </div>
-        <button className="roll-dice">Roll</button>
+        <h2>How to play?</h2>
+        <p className="instructions-staring-page">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <button className="home-buttons">Play Game</button>
+        <button className="home-buttons">View LeaderBoard</button>
       </main>
     </>
   )
