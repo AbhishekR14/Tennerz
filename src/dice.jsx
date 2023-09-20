ab1980269e4648028b26e321e715b81bf822888a
AbhishekR14 <aranjan0288@gmail.com>
2023-09-20 16:56:40 +0530

    roll dice and select dice funationality working

--- src/dice.jsx
+++ src/dice.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import '../Stylesheets/dice.css'
 export default function Dice(props){
+    
+    const style = {
+        backgroundColor : props.isSelected ? "#8afccc" : "white"
+    }
+
     return(
-        <div className="single-dice">
+        <div style={style}className="single-dice" onClick={() => props.onClick(props.id)}>
             {props.value}
         </div>
     )
--- src/tenzies.jsx
+++ src/tenzies.jsx
@@ -1,27 +1,61 @@
 import React from "react";
 import Dice from './dice.jsx'
+import { nanoid } from 'nanoid'
 import '../Stylesheets/tenzies.css'
 import '../Stylesheets/index.css'
+
 export default function tenzies(){
-    return (
-      <>
-        <main>
-          <h1 className="title">Tenzies</h1>
-          <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-          <div className="dice-grid">
-          <Dice value="1"/>
-          <Dice value="2"/>
-          <Dice value="3"/>
-          <Dice value="4"/>
-          <Dice value="5"/>
-          <Dice value="6"/>
-          <Dice value="1"/>
-          <Dice value="1"/>
-          <Dice value="1"/>
-          <Dice value="1"/>
-          </div>
-          <button className="roll-dice">Roll</button>
-        </main>
-      </>
-    )
+
+  const [dices , setDices] = React.useState(generateRandomDiceArray());
+
+  function generateRandomDiceArray(){
+    let diceArray = [];
+    for(let i = 0 ; i < 10 ; i++){
+      diceArray.push(generateRandomDice());
+    }
+    return diceArray
+  }
+
+  function generateRandomDice(){
+    return {
+      id: nanoid(),
+      value: Math.floor(Math.random() * 6) + 1,
+      isSelected : false
+    }
+  }
+
+  function rollDice(){
+    setDices(oldDice => oldDice.map(dice => {
+      return dice.isSelected ? dice : generateRandomDice()
+    }));
+  }
+
+  function changeSelected(id){
+    setDices(oldDice => oldDice.map(die => {
+      return die.id === id ? {...die, isSelected: !die.isSelected} : die
+    }))
+  }
+
+  const diceElements = dices.map(dice => {
+    return <Dice 
+              key ={dice.id}
+              id = {dice.id}
+              value={dice.value} 
+              isSelected={dice.isSelected}
+              onClick={changeSelected}
+            />
+  })
+
+  return (
+    <>
+      <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="dice-grid">
+          {diceElements}
+        </div>
+        <button className="roll-dice" onClick={rollDice}>Roll</button>
+      </main>
+    </>
+  )
 }
